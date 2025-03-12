a6509d242ec78272f079303e0d65c73f87a1940f
Duke <hjxduke080111@gmail.com>
2025-03-12 16:58:08 +0700

    Minor changes

--- src/reportWebVitals.js
+++ src/reportWebVitals.js
@@ -1,12 +1,7 @@
-const reportWebVitals = (onPerfEntry) => {
-  if (onPerfEntry && onPerfEntry instanceof Function) {
-    import('web-vitals').then(({ getCLS, getFID, getFCP, getLCP, getTTFB }) => {
-      getCLS(onPerfEntry);
-      getFID(onPerfEntry);
-      getFCP(onPerfEntry);
-      getLCP(onPerfEntry);
-      getTTFB(onPerfEntry);
-    });
+const reportWebVitals = () => {
+  // This is a minimal implementation that can be expanded later if analytics are needed
+  if (process.env.NODE_ENV !== 'production') {
+    console.log('Web Vitals reporting is disabled in production');
   }
 };
 
